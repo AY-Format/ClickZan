0d2dc8cb2e41e5900ff37ee61debfa68c3f96ee6
BuildTools <unconfigured@null.spigotmc.org>
2019-06-06 20:57:39 +0800

    finish

--- app/src/main/java/com/example/clickzan/MainActivity.java
+++ app/src/main/java/com/example/clickzan/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.clickzan;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import com.example.clickzan.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyViewModel myViewModel;
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        myViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
+        activityMainBinding.setData(myViewModel);
+        activityMainBinding.setLifecycleOwner(this);
     }
 }
